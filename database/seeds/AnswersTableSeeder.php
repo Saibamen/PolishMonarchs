6b6b509bab4b52b6f74e6a8011a4661b7002fb56
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2016-11-02 21:20:50 +0100

    Końcowe uzupełnienie bazy danych

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -7,9 +7,146 @@ class AnswersTableSeeder extends Seeder {
 
     public function run() {
         $roles = [
+            // Należał do dynastii Piastów?
             ["question_id" => 1, "person_id" => 1, "answer" => 1],
+            ["question_id" => 1, "person_id" => 2, "answer" => 1],
+            ["question_id" => 1, "person_id" => 3, "answer" => 1],
+            ["question_id" => 1, "person_id" => 4, "answer" => 1],
+            ["question_id" => 1, "person_id" => 6, "answer" => 1],
+            ["question_id" => 1, "person_id" => 7, "answer" => 1],
+
+            // Miał przydomek?
+            ["question_id" => 2, "person_id" => 1, "answer" => 1],
+            ["question_id" => 2, "person_id" => 2, "answer" => 1],
             ["question_id" => 2, "person_id" => 3, "answer" => 1],
-            ["question_id" => 3, "person_id" => 2, "answer" => 1],
+            ["question_id" => 2, "person_id" => 6, "answer" => 1],
+            ["question_id" => 2, "person_id" => 7, "answer" => 1],
+            ["question_id" => 2, "person_id" => 8, "answer" => 1],
+            ["question_id" => 2, "person_id" => 9, "answer" => 1],
+            ["question_id" => 2, "person_id" => 11, "answer" => 1],
+            ["question_id" => 2, "person_id" => 12, "answer" => 1],
+            ["question_id" => 2, "person_id" => 13, "answer" => 1],
+            ["question_id" => 2, "person_id" => 14, "answer" => 1],
+            ["question_id" => 2, "person_id" => 15, "answer" => 1],
+            ["question_id" => 2, "person_id" => 16, "answer" => 1],
+            ["question_id" => 2, "person_id" => 18, "answer" => 1],
+            ["question_id" => 2, "person_id" => 25, "answer" => 1],
+            ["question_id" => 2, "person_id" => 26, "answer" => 1],
+            ["question_id" => 2, "person_id" => 27, "answer" => 1],
+
+            // Należał do dynastii Jagiellonów?
+            ["question_id" => 3, "person_id" => 10, "answer" => 1],
+            ["question_id" => 3, "person_id" => 11, "answer" => 1],
+            ["question_id" => 3, "person_id" => 12, "answer" => 1],
+            ["question_id" => 3, "person_id" => 13, "answer" => 1],
+            ["question_id" => 3, "person_id" => 14, "answer" => 1],
+            ["question_id" => 3, "person_id" => 15, "answer" => 1],
+            ["question_id" => 3, "person_id" => 16, "answer" => 1],
+
+            // Była kobietą?
+            ["question_id" => 4, "person_id" => 9, "answer" => 1],
+            ["question_id" => 4, "person_id" => 18, "answer" => 1],
+
+            // Był królem elekcyjnym?
+            ["question_id" => 5, "person_id" => 17, "answer" => 1],
+            ["question_id" => 5, "person_id" => 18, "answer" => 1],
+            ["question_id" => 5, "person_id" => 19, "answer" => 1],
+            ["question_id" => 5, "person_id" => 20, "answer" => 1],
+            ["question_id" => 5, "person_id" => 21, "answer" => 1],
+            ["question_id" => 5, "person_id" => 22, "answer" => 1],
+            ["question_id" => 5, "person_id" => 23, "answer" => 1],
+            ["question_id" => 5, "person_id" => 24, "answer" => 1],
+            ["question_id" => 5, "person_id" => 25, "answer" => 1],
+            ["question_id" => 5, "person_id" => 26, "answer" => 1],
+            ["question_id" => 5, "person_id" => 27, "answer" => 1],
+            ["question_id" => 5, "person_id" => 28, "answer" => 1],
+
+            // Nie był Polakiem?
+            ["question_id" => 6, "person_id" => 5, "answer" => 1],
+            ["question_id" => 6, "person_id" => 8, "answer" => 1],
+            ["question_id" => 6, "person_id" => 9, "answer" => 1],
+            ["question_id" => 6, "person_id" => 17, "answer" => 1],
+            ["question_id" => 6, "person_id" => 19, "answer" => 1],
+            ["question_id" => 6, "person_id" => 20, "answer" => 1],
+            ["question_id" => 6, "person_id" => 25, "answer" => 1],
+            ["question_id" => 6, "person_id" => 27, "answer" => 1],
+
+            // Nie zmarł w Polsce?
+            ["question_id" => 7, "person_id" => 5, "answer" => 1],
+            ["question_id" => 7, "person_id" => 8, "answer" => 1],
+            ["question_id" => 7, "person_id" => 11, "answer" => 1],
+            ["question_id" => 7, "person_id" => 17, "answer" => 1],
+            ["question_id" => 7, "person_id" => 22, "answer" => 1],
+            ["question_id" => 7, "person_id" => 26, "answer" => 1],
+            ["question_id" => 7, "person_id" => 27, "answer" => 1],
+            ["question_id" => 7, "person_id" => 28, "answer" => 1],
+
+            // Rządził przez długi okres czasu (ponad 30 lat)?
+            ["question_id" => 8, "person_id" => 7, "answer" => 1],
+            ["question_id" => 8, "person_id" => 10, "answer" => 1],
+            ["question_id" => 8, "person_id" => 12, "answer" => 1],
+            ["question_id" => 8, "person_id" => 15, "answer" => 1],
+            ["question_id" => 8, "person_id" => 20, "answer" => 1],
+            ["question_id" => 8, "person_id" => 25, "answer" => 1],
+            ["question_id" => 8, "person_id" => 27, "answer" => 1],
+            ["question_id" => 8, "person_id" => 28, "answer" => 1],
+
+            // Panował przez ponad połowę swojego życia?
+            ["question_id" => 9, "person_id" => 7, "answer" => 1],
+            ["question_id" => 9, "person_id" => 10, "answer" => 1],
+            ["question_id" => 9, "person_id" => 11, "answer" => 1],
+            ["question_id" => 9, "person_id" => 12, "answer" => 1],
+            ["question_id" => 9, "person_id" => 15, "answer" => 1],
+            ["question_id" => 9, "person_id" => 16, "answer" => 1],
+            ["question_id" => 9, "person_id" => 20, "answer" => 1],
+            ["question_id" => 9, "person_id" => 25, "answer" => 1],
+
+            // Władał również innym krajem (poza Polską i Litwą)?
+            ["question_id" => 10, "person_id" => 1, "answer" => 1],
+            ["question_id" => 10, "person_id" => 5, "answer" => 1],
+            ["question_id" => 10, "person_id" => 8, "answer" => 1],
+            ["question_id" => 10, "person_id" => 11, "answer" => 1],
+            ["question_id" => 10, "person_id" => 17, "answer" => 1],
+            ["question_id" => 10, "person_id" => 20, "answer" => 1],
+            ["question_id" => 10, "person_id" => 21, "answer" => 1],
+            ["question_id" => 10, "person_id" => 25, "answer" => 1],
+            ["question_id" => 10, "person_id" => 26, "answer" => 1],
+
+            // Dokładna data śmierci jest nieznana?
+            ["question_id" => 11, "person_id" => 3, "answer" => 1],
+
+            // Był bezpośrednim potomkiem króla/królowej?
+            ["question_id" => 12, "person_id" => 2, "answer" => 1],
+            ["question_id" => 12, "person_id" => 7, "answer" => 1],
+            ["question_id" => 12, "person_id" => 9, "answer" => 1],
+            ["question_id" => 12, "person_id" => 11, "answer" => 1],
+            ["question_id" => 12, "person_id" => 12, "answer" => 1],
+            ["question_id" => 12, "person_id" => 13, "answer" => 1],
+            ["question_id" => 12, "person_id" => 14, "answer" => 1],
+            ["question_id" => 12, "person_id" => 15, "answer" => 1],
+            ["question_id" => 12, "person_id" => 16, "answer" => 1],
+
+            // Należał do dynsatii Wazów?
+            ["question_id" => 13, "person_id" => 20, "answer" => 1],
+            ["question_id" => 13, "person_id" => 21, "answer" => 1],
+            ["question_id" => 13, "person_id" => 22, "answer" => 1],
+
+            // Przejął koronę po bracie?
+            ["question_id" => 14, "person_id" => 12, "answer" => 1],
+            ["question_id" => 14, "person_id" => 14, "answer" => 1],
+            ["question_id" => 14, "person_id" => 15, "answer" => 1],
+
+            // Urodził się w stolicy?
+            ["question_id" => 15, "person_id" => 11, "answer" => 1],
+            ["question_id" => 15, "person_id" => 12, "answer" => 1],
+            ["question_id" => 15, "person_id" => 13, "answer" => 1],
+            ["question_id" => 15, "person_id" => 14, "answer" => 1],
+            ["question_id" => 15, "person_id" => 16, "answer" => 1],
+            ["question_id" => 15, "person_id" => 18, "answer" => 1],
+            ["question_id" => 15, "person_id" => 22, "answer" => 1],
+
+            // Na jego cześć powstała marka wódki i papierosów?
+            ["question_id" => 16, "person_id" => 24, "answer" => 1],
         ];
 
         foreach($roles as $role) {
--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -7,9 +7,23 @@ class QuestionsTableSeeder extends Seeder {
 
     public function run() {
         $roles = [
-            ["name" => "Czy ucięli mu głowę?"],
-            ["name" => "Czy to on 'mówił ośmioma językami'?"],
-            ["name" => "Czy urodził się na terenach polskich?"],
+            ["id" => 1, "name" => "Należał do dynastii Piastów?"],
+            ["id" => 2, "name" => "Miał przydomek?"],
+            ["id" => 3, "name" => "Należał do dynastii Jagiellonów?"],
+            ["id" => 4, "name" => "Była kobietą?"],
+            ["id" => 5, "name" => "Był królem elekcyjnym?"],
+            ["id" => 6, "name" => "Nie był Polakiem?"],
+            ["id" => 7, "name" => "Nie zmarł w Polsce?"],
+            ["id" => 8, "name" => "Rządził przez długi okres czasu (ponad 30 lat)?"],
+            ["id" => 9, "name" => "Panował przez ponad połowę swojego życia?"],
+            ["id" => 10, "name" => "Władał również innym krajem (poza Polską i Litwą)?"],
+            ["id" => 11, "name" => "Dokładna data śmierci jest nieznana?"],
+            ["id" => 12, "name" => "Był bezpośrednim potomkiem króla/królowej?"],
+            ["id" => 13, "name" => "Należał do dynsatii Wazów?"],
+            ["id" => 14, "name" => "Przejął koronę po bracie?"],
+            ["id" => 15, "name" => "Urodził się w stolicy?"],
+            ["id" => 16, "name" => "Na jego cześć powstała marka wódki i papierosów?"],
+
         ];
 
         foreach($roles as $role) {
